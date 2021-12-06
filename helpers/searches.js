170f0a83f0a331b0761327e129d4dfa9a7486296
mllimo <ar.guijarro.contreras@gmail.com>
2021-12-06 18:36:44 +0000

    Conectando con APi Mapbox

--- helpers/searches.js
+++ helpers/searches.js
@@ -1,14 +1,36 @@
-
+const axios = require('axios');
 
 class Searches {
   history = [];
-  
+
   constructor() {
-  
+
+  }
+
+  get paramsMapBox() {
+    return {
+      'access_token': 'pk.eyJ1IjoibWxsaW1vIiwiYSI6ImNrd3V5djlndzF1eHAycHAzbGZjbmtsMHMifQ.zVKVa0FbNJL__3MMVKYOwg',
+      'limit': '5',
+      'language': 'en'
+    }
   }
 
   async city(place = '') {
-    
+    try {
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?`,
+        params: this.paramsMapBox
+      });
+
+      const response = await instance.get();
+      console.log(response);
+
+      //this.history.push(place);
+
+    } catch (error) {
+      console.error(error);
+    }
+
   }
 }
 
--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const io = require('./helpers/io');
+const Searches = require('./helpers/searches');
+require('dotenv').config()
 
 async function main() {
   let option;
-  
+  let searches = new Searches;
   do {
     option = await io.showMenu();
-    console.log(option);
+    
+    switch (option) {
+      case 1:
+        // Show message
+        const place = await io.readInput('City: '); 
+
+        // Search the place
+        searches.city(place);
+
+        // Choose the place
+
+        // Weather
+
+        // Show results
+        console.log('\nCity information'.green);
+        console.log('\nCity: ',);
+        console.log('\Lat: ',);
+        console.log('\Lng: ',);
+        console.log('\nTemperature: ',);
+        console.log('\nMinimum: ',);
+        console.log('\Maximum: ',);
+        break;
+      
+      case 2:
+        break;
+    }
+
     if (option !== 0) await io.pause();
 
   } while(option !== 0);
